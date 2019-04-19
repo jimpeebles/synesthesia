d7fb45aa52754afdbfb514eddf07baa3fdece5cc
jimpeebles <james@concepttoproduct.us>
2019-04-19 13:20:23 -0400

    Getting somewhere, need to grab correct key

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container" :style="{'--r': this.r,'--g': this.g,'--b': this.b, '--a':this.a}">
-    <input @input="colorMash($event)" class="thebox" type="text">
+    <input @input="colorMash($event)" v-model="txtin" class="thebox" type="text">
   </div>
 </template>
 
@@ -11,17 +11,32 @@ export default {
       r: 50,
       g: 146,
       b: 100,
+      txtin: "",
       currentTimeSinceLastKey: 0,
       minTSL: 0,
       maxTSL: 0,
-      a: 0
+      a: 0,
+      buffer: [100, 100, 100]
     };
   },
   methods: {
-    colorMash($event) {}
-  },
-  alpha(currentTSL, minTSL, maxTSL) {
-    this.alphaValue = (currentTSL - minTSL) / (maxTSL - minTSL);
+    colorMash(event) {
+      let keycode = this.txtin.charCodeAt(0);
+      this.txtin = event.target.value.charAt(0);
+      let normalized = this.normalize(keycode, 0, 177);
+      let rgbval = normalized * 255;
+      this.buffer.push(rgbval);
+      // this.buffer[0] = this.buffer[1];
+      // this.buffer[1] = this.buffer[2];
+      // this.buffer[2] = normalized * 255;
+      console.log(this.buffer.shift());
+      this.r = this.buffer[0];
+      this.g = this.buffer[1];
+      this.b = this.buffer[2];
+    },
+    normalize(val, min, max) {
+      return (val - min) / (max - min);
+    }
   }
 };
 </script>
